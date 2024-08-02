5339d2ac77ed1e6b0bbd472f2d8c8f07f9d94229
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2024-08-02 19:17:57 +1200

    Added ball

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -4,14 +4,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 @SuppressWarnings("serial")
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY  = 5;
 	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
 	}
  
 	@Override
@@ -35,6 +40,17 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.white);
+		g.fillRect(20, 20, 100, 100);
+	}
+	
+	private void update() {
 		
 	}
 	
