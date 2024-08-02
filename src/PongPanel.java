32d2361e522ffe91b88734c7617ade1b8164792b
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2024-08-02 19:33:18 +1200

    Added positioning

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
 @SuppressWarnings("serial")
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
@@ -39,19 +41,61 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		update();
+		repaint();
 		
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.white);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
+//		g.setColor(Color.white);
+//		g.fillRect(20, 20, 100, 100);
 	}
+	
+    private void paintDottedLine(Graphics g) {
+		 Graphics2D g2d = (Graphics2D) g.create();
+	     Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	     g2d.setStroke(dashed);
+	     g2d.setPaint(Color.WHITE);
+	     g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	     g2d.dispose();
+ }
 	
 	private void update() {
 		
 	}
 	
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
